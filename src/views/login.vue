2a83984581a243be90e3de94939bc62578878387
zhousijian <13553593242@163.com>
2020-01-12 21:37:31 +0800

    添加在密码框按回车键可以登录

--- src/views/login.vue
+++ src/views/login.vue
@@ -20,7 +20,7 @@
         </el-form-item>
         <!-- 密码框 -->
         <el-form-item prop="password">
-          <el-input v-model="loginForm.password" placeholder="请输入密码" prefix-icon='icon-key'></el-input>
+          <el-input v-model="loginForm.password" placeholder="请输入密码" prefix-icon='icon-key' @keyup.enter.native='Login'></el-input>
         </el-form-item>
         <el-form-item>
           <el-button type="primary" class="login-btn" @click="Login">登录</el-button>
