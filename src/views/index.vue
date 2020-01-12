453df89f92b78261580bc8e04f2c6dd2cd6657f1
zhousijian <13553593242@163.com>
2020-01-12 17:21:28 +0800

    Revert "完成登录时的判断"
    This reverts commit f56eb81b64540a3903b8808963bbc743c5da4147.

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,61 +1,15 @@
 <template>
   <div class="index">
-    <el-container>
-      <el-aside width="200px">Aside</el-aside>
-      <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
-      </el-container>
-    </el-container>
+      首页
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+
+}
 </script>
 
-<style lang="less" scoped>
-.index {
-  height: 100%;
-  .el-menu-admin:not(.el-menu--collapse) {
-    width: 200px;
-    min-height: 400px;
-  }
-  .el-container {
-    height: 100%;
-  }
-  .el-aside {
-    background-color: #545c64;
-  }
-  .el-header {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    background-color: #545c64;
-  }
-  .logo {
-    height:60px;
-    background: url(../assets/logo.png);
-    background-size: cover;
-    background-color: #fff;
-  }
-  .toggle-btn {
-    padding: 0 15px;
-    margin-left: -20px;
-    font-size: 36px;
-    color: white;
-    cursor: pointer;
-    line-height: 60px;
-    &:hover {
-      background-color: #4292CF;
-    }
-  }
-  .system-title {
-    font-size: 28px;
-    color: white;
-  }
-  .welcome{
-    color: white;
-  }
-}
+<style>
+
 </style>
