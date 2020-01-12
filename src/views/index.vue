16e585f04278c14f66f501f4f5ec540eed78b63e
zhousijian <13553593242@163.com>
2020-01-12 21:19:42 +0800

    完成首页制作
    添加组件，配置路由，制作首页组件，左侧导航菜单，顶部导航

--- src/views/index.vue
+++ src/views/index.vue
@@ -1,10 +1,57 @@
 <template>
   <div class="index">
     <el-container>
-      <el-aside width="200px">Aside</el-aside>
+      <el-aside width="200px">
+        <el-menu
+          :router='true'
+          default-active="/index/articles"
+          class="el-menu-vertical-demo"
+          background-color="#545c64"
+          text-color="#fff"
+          active-text-color="#ffd04b"
+        >
+          <el-submenu index="1">
+            <template slot="title">
+              <i class="el-icon-user-solid"></i>
+              <span>用户管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="1-1" class="el-icon-user">&nbsp;用户列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="2">
+            <template slot="title">
+              <i class="el-icon-document"></i>
+              <span>文章管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="/index/articles" class="el-icon-reading">&nbsp;文章列表</el-menu-item>
+              <el-menu-item index="2-2" class="el-icon-document-add">&nbsp;文章发布</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+          <el-submenu index="3">
+            <template slot="title">
+              <i class="el-icon-finished"></i>
+              <span>栏目管理</span>
+            </template>
+            <el-menu-item-group>
+              <el-menu-item index="3-1" class="el-icon-chat-line-square">&nbsp;栏目列表</el-menu-item>
+            </el-menu-item-group>
+          </el-submenu>
+        </el-menu>
+      </el-aside>
       <el-container>
-        <el-header>Header</el-header>
-        <el-main>Main</el-main>
+        <el-header>
+          <span class="el-icon-s-fold toggle-btn"></span>
+          <span class="system-title">黑马头条后台管理系统</span>
+          <div class="welcome">
+            <span>欢迎你：41期的小伙伴</span> &nbsp;&nbsp;
+            <span>退出</span>
+          </div>
+        </el-header>
+        <el-main>
+          <router-view></router-view>
+          </el-main>
       </el-container>
     </el-container>
   </div>
@@ -34,7 +81,7 @@ export default {}
     background-color: #545c64;
   }
   .logo {
-    height:60px;
+    height: 60px;
     background: url(../assets/logo.png);
     background-size: cover;
     background-color: #fff;
@@ -47,15 +94,18 @@ export default {}
     cursor: pointer;
     line-height: 60px;
     &:hover {
-      background-color: #4292CF;
+      background-color: #4292cf;
     }
   }
   .system-title {
     font-size: 28px;
     color: white;
   }
-  .welcome{
+  .welcome {
     color: white;
   }
+}
+.el-menu-vertical-demo {
+  border-right: 0;
 }
 </style>
